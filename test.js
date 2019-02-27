f33c4c506b4a5250e1716eff565e505a6c5d1866
Rumiantsev Oleksii <rumiantsev.oleksii@gmail.com>
2019-02-27 19:08:30 +0200

    Add unit tests for impress Application class
    Closes: https://github.com/metarhia/impress/issues/1023

--- test.js
+++ test.js
@@ -3,8 +3,33 @@
 require('./lib/core');
 
 if (impress.isMaster) {
+  const unittestsPath = api.path.join(__dirname, 'unittests');
+
   impress.on('testsFinished', errors => {
-    impress.shutdown(errors ? 1 : 0);
+    if (errors) {
+      impress.shutdown(1);
+      return;
+    }
+
+    api.fs.readdir(unittestsPath, (err, files) => {
+      if (err) {
+        console.error(err);
+        impress.shutdown(1);
+        return;
+      }
+
+      const unittests = api.test.test('Impress unittests');
+
+      files.forEach(name => {
+        const path = api.path.join(unittestsPath, name);
+        const testFn = require(path);
+        unittests.test(name, testFn);
+      });
+
+      unittests.on('done', () => {
+        impress.shutdown(unittests.success ? 0 : 1);
+      });
+    });
   });
 }
 
---
+++ unittests/application.getMethods.js
@@ -0,0 +1,11 @@
+'use strict';
+
+module.exports = test => {
+  const dir = api.path.join(impress.moduleDir, 'applications/example');
+  const app = new impress.Application('example', dir);
+  app.on('started', () => {
+    const methods = app.getMethods('interfaceName');
+    test.strictEqual(methods.sort(), ['methodName', 'sendEvent']);
+    test.end();
+  });
+};
